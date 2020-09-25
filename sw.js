049f82cd3e6c0942a49601cbbfec733d2875e5cf
devos3195 <62845593+devos3195@users.noreply.github.com>
2020-09-25 13:32:39 +0900

    dfsdfsdf

---
+++ sw.js
@@ -0,0 +1,14 @@
+"use strict";
+// && 'PushManager' in window
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        console.log('1');
+        navigator.serviceWorker.register('https://ohtaeyang95.github.io/pushtest/service-worker.js')
+            .then(function (success) {
+                console.log('[Service Worker 등록 완료]', success);
+            })
+            .catch(function (error) {
+                console.log('[Service Worker 등록 실패]', error);
+            });
+    });
+}
