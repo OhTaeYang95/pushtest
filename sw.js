019bb64ccecb6f700d2ffb5e80732af6c1e24bf2
devos3195 <62845593+devos3195@users.noreply.github.com>
2020-09-29 00:00:24 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,9 +2,11 @@
 // && 'PushManager' in window
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        console.log('13');
+        console.log('14');
         navigator.serviceWorker.register('https://ohtaeyang95.github.io/pushtest/OneSignalSDKUpdaterWorker.js')
         navigator.serviceWorker.register('https://ohtaeyang95.github.io/pushtest/service-worker.js')
+        navigator.serviceWorker.register('https://ohtaeyang95.github.io/OneSignalSDKUpdaterWorker.js')
+        navigator.serviceWorker.register('https://ohtaeyang95.github.io/OneSignalSDKWorker.js')
             .then(function (success) {
                 console.log('[Service Worker Sucessed]', success);
             })
