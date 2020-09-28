72c440e70ce8f1bc00e2b9ab715213a1a2033829
devos3195 <62845593+devos3195@users.noreply.github.com>
2020-09-28 17:31:32 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // && 'PushManager' in window
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        console.log('4');
+        console.log('5');
         navigator.serviceWorker.register('https://ohtaeyang95.github.io/pushtest/service-worker.js')
             .then(function (success) {
                 console.log('[Service Worker 등록 완료]', success);
