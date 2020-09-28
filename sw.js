2338c5ea5e7bb81b62da1b4dba0eff3a0c692534
devos3195 <62845593+devos3195@users.noreply.github.com>
2020-09-28 23:37:27 +0900

    assaas

--- sw.js
+++ sw.js
@@ -2,14 +2,14 @@
 // && 'PushManager' in window
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        console.log('11');
+        console.log('12');
         navigator.serviceWorker.register('https://ohtaeyang95.github.io/pushtest/OneSignalSDKUpdaterWorker.js')
         navigator.serviceWorker.register('https://ohtaeyang95.github.io/pushtest/service-worker.js')
             .then(function (success) {
-                console.log('[Service Worker 등록 완료]', success);
+                console.log('[Service Worker Sucessed]', success);
             })
             .catch(function (error) {
-                console.log('[Service Worker 등록 실패]', error);
+                console.log('[Service Worker Failed]', error);
             });
     });
 }
